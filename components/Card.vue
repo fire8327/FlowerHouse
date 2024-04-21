181f9dbd625baa4f8a1aec42f69231bba4f00b0c
fire8327 <fire83274@gmail.com>
2024-04-22 00:28:00 +0300

    fix

--- components/Card.vue
+++ components/Card.vue
@@ -1,6 +1,8 @@
 <template>
      <div class="flex flex-col gap-6">
-        <img src="https://static.tildacdn.com/stor6135-3938-4736-b038-313639336661/93178138.jpg" alt="" class="rounded-xl">
+        <div class="overflow-hidden rounded-xl">
+            <img src="https://static.tildacdn.com/stor6135-3938-4736-b038-313639336661/93178138.jpg" alt="" class="transition-all duration-500 hover:scale-125">
+        </div>
         <p class="text-2xl font-Comfortaa">Керамический горшок «Серый камень»</p>
         <p class="text-xl font-semibold">3 500₽</p>
         <div class="flex items-center gap-2 max-md:flex-col text-lg">
@@ -9,7 +11,7 @@
                 <p>{{ cardCount }}</p>
                 <button class="text-2xl" @click="plusCount">+</button>
             </div>    
-            <button class="md:w-3/5 px-4 py-2 text-center bg-[#569E0B] transition-all duration-500 hover:text-[#665E5E] text-white rounded-xl">В корзину</button>
+            <button class="md:w-3/5 px-4 py-2 text-center bg-[#569E0B] transition-all duration-500 hover:bg-[#665E5E] text-white rounded-xl">В корзину</button>
         </div>
     </div>
 </template>
