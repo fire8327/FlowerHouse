40d0f0be398349424274e54a84b9091191929b0e
fire8327 <fire83274@gmail.com>
2024-04-22 01:51:42 +0300

    upd

--- pages/catalog.vue
+++ pages/catalog.vue
@@ -15,11 +15,8 @@
             <p>Каталог</p>
         </div>
     </div>
-    <div class="flex flex-col gap-6">
-        <NuxtLink to="/catalog" class="text-3xl font-Comfortaa text-[#569E0B]/70">Горшки</NuxtLink>
-        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
-           <Card v-for="n in 6"></Card>
-        </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
+       <Card v-for="n in 6"></Card>
     </div>
 </template>
 
