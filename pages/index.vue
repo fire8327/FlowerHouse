55d4ac1ec673ea54cc8af9ff89da2896bbad8946
fire8327 <fire83274@gmail.com>
2024-04-22 00:16:44 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -63,7 +63,7 @@
     <div class="flex flex-col gap-6">
         <p class="text-3xl font-Comfortaa text-[#569E0B]/70">Наши новинки</p>
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
-           <Card></Card>
+           <Card v-for="n in 8"></Card>
         </div>
     </div>
     <div class="flex flex-col gap-6">
