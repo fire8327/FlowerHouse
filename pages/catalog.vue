48e546eec934689af71b6f11bfaf713a595eb6f3
fire8327 <fire83274@gmail.com>
2024-04-22 20:13:28 +0300

    upd catalog

--- pages/catalog.vue
+++ pages/catalog.vue
@@ -15,8 +15,21 @@
             <p>Каталог</p>
         </div>
     </div>
+    <FormKit type="form" :actions="false" messages-class="hidden" form-class="flex items-center lg:justify-between gap-6 max-lg:flex-col p-4 shadow-[0px_0px_13px_-7px_black] rounded-xl">
+        <div class="flex items-center gap-6 max-lg:flex-col max-lg:w-full">
+            <div class="flex items-center gap-2 max-lg:w-full">
+                <FormKit type="text" name="От" outer-class="w-1/2" input-class="px-4 py-2 border border-[#569E0B]/70 rounded-l-xl focus:outline-none w-full" placeholder="От"/>
+                <FormKit type="text" name="До" outer-class="w-1/2" input-class="px-4 py-2 border border-[#569E0B]/70 rounded-r-xl focus:outline-none w-full" placeholder="До"/>
+            </div>
+            <FormKit type="select" :options="['Все','Горшки','Грунты','Удобрения']" outer-class="max-lg:w-full" input-class="px-4 py-2 border border-[#569E0B]/70 rounded-xl focus:outline-none w-full"/>
+        </div>
+        <div class="flex items-center gap-4">
+            <button class="px-4 py-2 w-fit bg-[#569E0B] text-white rounded-xl">Применить</button>
+            <button class="px-4 py-2 w-fit border border-[#569E0B] text-[#569E0B] rounded-xl">Отменить</button>
+        </div>    
+    </FormKit>
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
-       <Card v-for="n in 6"></Card>
+       <Card v-for="n in 12"></Card>
     </div>
 </template>
 
