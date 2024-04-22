10af034d279d91495b549653f862d5768d37ceaa
fire8327 <fire83274@gmail.com>
2024-04-22 20:32:59 +0300

    upd

--- components/Card.vue
+++ components/Card.vue
@@ -5,13 +5,13 @@
         </div>
         <p class="text-2xl font-Comfortaa">Керамический горшок «Серый камень»</p>
         <p class="text-xl font-semibold">3 500₽</p>
-        <div class="flex items-center gap-2 max-md:flex-col text-lg">
+        <div class="flex items-center gap-2 text-lg">
             <div class="flex items-center justify-center gap-6 px-4 py-1.5 grow rounded-xl border border-[#665E5E]">
                 <button class="text-2xl" @click="minusCount">-</button>
                 <p>{{ cardCount }}</p>
                 <button class="text-2xl" @click="plusCount">+</button>
             </div>    
-            <button class="md:w-3/5 px-4 py-2 text-center bg-[#569E0B] transition-all duration-500 hover:bg-[#665E5E] text-white rounded-xl">В корзину</button>
+            <button class="w-3/5 px-4 py-2 text-center bg-[#569E0B] transition-all duration-500 hover:bg-[#665E5E] text-white rounded-xl">В корзину</button>
         </div>
     </div>
 </template>
